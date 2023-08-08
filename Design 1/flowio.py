b29b30d5933c9300d54c8eb1bf743177728efa0a
Andylenguyen237 <lenguyenlinhdan02@gmail.com>
2023-08-08 22:24:04 +1000

    update on flowio and initially implement flowkit

--- Design 1/flowio.py
+++ Design 1/flowio.py
@@ -1,13 +1,13 @@
 import os
 import glob
 import pandas as pd
-from flowkit import FCMeasurement, io
+from FlowCytometryTools import FCMeasurement
 
 # Function to parse FCS files in a folder and return data as a DataFrame
 def parse_fcs_files(folder_path):
     data_frames = []
     for fcs_file in glob.glob(os.path.join(folder_path, "*.fcs")):
-        sample = FCMeasurement(fcs_file)
+        sample = FCMeasurement(ID=os.path.basename(fcs_file), datafile=fcs_file)
         data_frames.append(sample.data)
     merged_data = pd.concat(data_frames, ignore_index=True)
     return merged_data
@@ -17,13 +17,13 @@ input_folder = "/Users/andylenguyen/Documents/Genomics-Metadata-Multiplexing/Dat
 #input_folder = input("Enter the path to the folder containing FCS files: ")
 
 # Parse FCS files and merge data
-output_folder = "/Users/andylenguyen/Documents/Genomics-Metadata-Multiplexing/Design 1/flowkit_results"
-# Parse FCS files and merge data
+output_folder = "/Users/andylenguyen/Documents/Genomics-Metadata-Multiplexing/Design 1/flowIO_results"
+
 try:
     merged_data = parse_fcs_files(input_folder)
 
-    # Save merged data to an Excel file
-    output_file = "merged_data.xlsx"
+    # Generate a unique output filename
+    output_file = os.path.join(output_folder, "merged_data.xlsx")
     merged_data.to_excel(output_file, index=False)
     print(f"Merged data saved to {output_file}")
 except Exception as e:

369574785123a89cd68c4c7aba272c9045076d71
NandiRuan <101269247+BrandiRuan@users.noreply.github.com>
2024-02-16 20:29:17 +1100

    Delete Shiny-app/app_v4/install.R

--- Shiny-app/app_v4/install.R
+++
@@ -1,4 +0,0 @@
-packages <- c("DT", "shinycssloaders", "reticulate", "shiny", "RColorBrewer")
-newPackages <- packages[!(packages %in% installed.packages()[,"Package"])]
-if(length(newPackages)) install.packages(newPackages)
-

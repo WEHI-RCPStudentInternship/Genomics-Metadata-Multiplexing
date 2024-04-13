efa96d67219400c258fe594ab25f0800b3a2332e
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-04-14 09:51:59 +1000

    Update global.R

--- gmm/R/global.R
+++ gmm/R/global.R
@@ -2,8 +2,8 @@ library(shiny)
 library(shinythemes)
 
 onStart <- function() {
-    # Set any options or perform initializations here
-    options(device.ask.default = FALSE)
+    # Set any options or perform initialisations here
+    options(device.ask.default = FALSE, shiny.autoreload = TRUE)
     
     # For example, pre-load data or set global variables if not already done in global.R
     # load('data/mydata.RData') # Hypothetical example

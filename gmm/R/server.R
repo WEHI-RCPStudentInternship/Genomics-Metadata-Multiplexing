e69ae7ff1e29aa7be41ba2de651517104e7e63b4
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-04-21 22:45:23 +1000

    staged the progress

--- gmm/R/server.R
+++ gmm/R/server.R
@@ -1,15 +1,8 @@
 library(shiny)
 library(shinyjs)
 
-source("R/utils/custom_file_input.R")
-source("R/server_components/tutorial_server.R")
-source("R/server_components/operations_server.R")
-source("R/server_components/utils.R")
+source('R/server_components/homePage.server.R')
 
 server <- function(input, output, session) {
-    
-    switchTab(input, output, session)
-
-    operationServer(input, output, session)
-    
+    homePageServerHandler(input, output, session)
 }
--- gmm/app.R
+++ gmm/app.R
@@ -1,12 +1,50 @@
 library(shiny)
+library(reticulate)
 
-# Source global configurations and definitions
-source('R/global.R')
+# Use the virtual environment
+venv_dir <- "./venv"
+use_virtualenv(venv_dir, required = TRUE)
 
-# Define and run the application
-if (interactive()) {
-    onStart()
-    
-    app <- shinyApp(ui=ui, server=server)
-    runApp(app)
+ui <- fluidPage(
+    titlePanel("GMM Data Processor"),
+    sidebarLayout(
+        sidebarPanel(
+            fileInput('plate_layout', 'Choose Plate Layout File:'),
+            fileInput('fcs_file', 'Choose FCS File:'),
+            fileInput('template_sheet', 'Choose Template Sheet File:'),
+            fileInput('primer_index', 'Choose Primer Index File:'),
+            actionButton("process", "Process Files")
+        ),
+        mainPanel(
+            tableOutput("dataOutput")
+        )
+    )
+)
+
+server <- function(input, output, session) {
+    observeEvent(input$process, {
+        req(input$plate_layout, input$fcs_file, input$template_sheet, input$primer_index)
+        
+        # Define the output file path
+        output_file_path <- tempfile(fileext = ".csv")
+        
+        # Build command to run the Python script
+        command <- sprintf("python ./fcs_converter.py --plate-layout %s --fcs-file %s --template-sheet %s --primer-index %s --output-file %s",
+                           input$plate_layout$datapath,
+                           input$fcs_file$datapath,
+                           input$template_sheet$datapath,
+                           input$primer_index$datapath,
+                           output_file_path)
+        
+        # Execute the command
+        system(command)
+        
+        # Read and display the output file
+        outputData <- read.csv(output_file_path)
+        output$dataOutput <- renderTable({
+            outputData
+        })
+    })
 }
+
+shinyApp(ui = ui, server = server)

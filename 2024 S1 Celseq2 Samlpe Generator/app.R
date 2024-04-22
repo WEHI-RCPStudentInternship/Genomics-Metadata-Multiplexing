fb986a0c2b7a591b8b69ce413b26e761eef8e6cb
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-04-22 14:52:56 +1000

    Update app.R

--- 2024 S1 Celseq2 Samlpe Generator/app.R
+++ 2024 S1 Celseq2 Samlpe Generator/app.R
@@ -1,22 +1,23 @@
 library(shiny)
 library(reticulate)
 
+# Ensure 'renv' is installed
 if (!requireNamespace("renv", quietly = TRUE)) {
     install.packages("renv")
 }
 
-# Load renv and restore packages
+# Load 'renv' and restore packages
 renv::load()  
 renv::restore()  
 
-# Define the virtual environment directory
+# Define and use the virtual environment
 venv_dir <- "./venv"
-
-# Use the virtual environment
 use_virtualenv(venv_dir, required = TRUE)
 reticulate::py_config()
 
+# Load UI and server definitions
 source("R/ui.R")
 source("R/server.R")
 
+# Launch the Shiny app
 shinyApp(ui = ui, server = server)

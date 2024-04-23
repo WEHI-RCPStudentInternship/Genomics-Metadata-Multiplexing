ff14b6072f4f7bf61e01e39aa8951edfc4733cc9
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-04-24 01:42:29 +1000

    staged
    Co-authored-by: Marek Cmero <7799828+mcmero@users.noreply.github.com>

--- setup.sh
+++ setup.sh
@@ -14,11 +14,4 @@ echo "Installing Python packages..."
 pip install -r requirements.txt  
 
 # Step 3: Install R packages
-echo "Installing R packages..."
-Rscript -e 'install.packages(c("shiny", "shinyjs", "shinythemes", "reticulate", "DT"))' 
-
-# Step 4: Start the application
-echo "Starting the application..."
-source app.R
-
 echo "Setup complete."

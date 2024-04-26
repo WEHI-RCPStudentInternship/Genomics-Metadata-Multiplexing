a14f64f168a7126f0eace70f98bda3ed7765317b
rNLKJA <62606765+rNLKJA@users.noreply.github.com>
2024-04-26 12:56:01 +1000

    Update app.R

--- app.R
+++ app.R
@@ -1,20 +1,35 @@
-# clear environment dependencies
+# Step 1: Setup the environment
+# Clear existing environment dependencies
 rm(list=ls())
 
-source('setup.R')
+# Source the setup script
+source('R/setup.R')
 
+# Load necessary libraries
 library(shiny)
 library(reticulate)
 
-# Define and use the virtual environment
+# Step 2: Load Python virtual environment
+# Define the virtual environment directory
 venv_dir <- "./venv"
+
+# Use the specified virtual environment
 use_virtualenv(venv_dir, required = TRUE)
+
+# Optional: Display Python configuration to verify correct environment
 reticulate::py_config()
 
+# Step 3: Define the Shiny app
 # Load UI and server definitions
 source("R/ui.R")
 source("R/server.R")
 
-# Launch the Shiny app
+# Define the Shiny app
+app <- shinyApp(ui = ui, server = server, options = list(launch.browser = TRUE))
+
+# Step 4: Run the Shiny app
+getwd()
 print("Starting GMM Shiny Application...")
-shinyApp(ui = ui, server = server)
+
+# Run the app
+shiny::runApp(app)
